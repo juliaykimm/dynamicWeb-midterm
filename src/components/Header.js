61bcb8b9c185c33057e6802c98b23bb8739afd14
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-10-14 10:37:14 -0400

    thanku for helping

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="Header">
+      <div>
+        <h1>Miyazaki Films</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/container/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SGdata from "./SGdata";
+import Header from "../components/Header";
+const NounDataKey = `1a87155ce08a419caef43fe86e7b400d`;
+
+function Home() {
+  console.log("are u here?");
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Home;
